67d23e1b083c89828df4097e402b1f3befd1047e
KaelLim <kaelsohappy1@gmail.com>
2023-10-30 16:56:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,107 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Document is ready");  // Debug: Check if DOM is ready
+const buttonContainer = document.getElementById('contact-n-buttonContainer');
+const accordionContainer = document.getElementById('contact-n-accordionContainer');
 
-    document.getElementById("btnLocal").addEventListener("click", function() {
-        console.log("Button clicked");  // Debug: Check if button is clicked
-        
-        fetch('https://raw.githubusercontent.com/KaelLim/country-by-continent/main/affiliate_modified.json')
-        .then(response => {
-            console.log("Fetch response received");  // Debug: Check if fetch receives a response
-            return response.json();
-        })
-        .then(data => {
-            console.log("Data received:", data);  // Debug: Check the fetched data
+// Fetch the data from the provided JSON URL
+fetch('https://raw.githubusercontent.com/KaelLim/country-by-continent/main/affiliate_modified.json')
+    .then(response => response.json())
+    .then(data => {
+        const uniqueContinents = [...new Set(data.map(item => item.continents))];
 
-            const localEntries = data.filter(entry => entry.continents === '本會');
-            console.log("Filtered local entries:", localEntries);  // Debug: Check the filtered local entries
-
-            // Clear existing content
-            const contentDiv = document.getElementById("content");
-            contentDiv.innerHTML = "";
-
-            // Generate a special table for "花蓮本會" and place it at the top
-            const hualienEntry = localEntries.find(entry => entry.affiliateName === "花蓮本會");
-            if (hualienEntry) {
-                const specialTable = document.createElement("table");
-                specialTable.innerHTML = "<tr><th>志業體名稱</th><th>志業體</th><th>志業體電話</th><th>志業體地址</th><th>志業體地圖</th></tr>";
-                const specialRow = specialTable.insertRow(-1);
-                specialRow.insertCell(0).innerHTML = hualienEntry.affiliateName;
-                specialRow.insertCell(1).innerHTML = hualienEntry.mission;
-                specialRow.insertCell(2).innerHTML = hualienEntry.phone1;
-                specialRow.insertCell(3).innerHTML = hualienEntry.fullAddress;
-                specialRow.insertCell(4).innerHTML = `<a href="https://www.google.com.tw/maps/place/${hualienEntry.fullAddress}" target="_blank">Google 地圖</a>`;
-                const specialTitle = document.createElement("h2");
-                specialTitle.innerHTML = "花蓮本會";
-                specialTitle.className = "specialAccordion";  // 新增這一行
-                contentDiv.appendChild(specialTitle);
-                contentDiv.appendChild(specialTable);
-            }
-
-            // Group by province
-            const groupedByProvince = localEntries.reduce((acc, entry) => {
-                const province = entry.province;
-                if (!acc[province]) {
-                    acc[province] = [];
-                }
-                acc[province].push(entry);
-                return acc;
-            }, {});
-
-            console.log("Grouped by province:", groupedByProvince);  // Debug: Check the grouping result
-
-            // Generate tables for each province
-            for (const [province, entries] of Object.entries(groupedByProvince)) {
-                console.log(`Generating table for province: ${province}`);  // Debug: Check which province is currently being processed
-                
-                const table = document.createElement("table");
-                table.className = "panel";  // Add the class here
-                table.innerHTML = "<tr><th>志業體名稱</th><th>志業體</th><th>志業體電話</th><th>志業體地址</th><th>志業體地圖</th></tr>";
-                
-                entries.forEach(entry => {
-                    const row = table.insertRow(-1);
-                    row.className = "myTableRowClass";  // Add the class here
-                    row.insertCell(0).innerHTML = entry.affiliateName;
-                    row.insertCell(1).innerHTML = entry.mission;
-                    row.insertCell(2).innerHTML = entry.phone1;
-                    row.insertCell(3).innerHTML = entry.fullAddress;
-                    row.insertCell(4).innerHTML = `<a href="https://www.google.com.tw/maps/place/${entry.fullAddress}" target="_blank">Google 地圖</a>`;
-                });
-
-                const provinceTitle = document.createElement("h2");
-                provinceTitle.className = "accordion";  // Add the class here
-                provinceTitle.innerHTML = province;
-
-                contentDiv.appendChild(provinceTitle);
-                contentDiv.appendChild(table);
-            }
-
-            // Add accordion event listeners AFTER all the accordions have been created
-            addAccordionEventListeners();
-        })
-        .catch(error => {
-            console.error('Error:', error);  // Debug: Check any errors
+        uniqueContinents.forEach(continent => {
+            const btn = document.createElement('button');
+            btn.className = "contact-n-button";
+            btn.innerText = continent;
+            btn.addEventListener('click', () => {
+                displayData(data, continent);
+            });
+            buttonContainer.appendChild(btn);
         });
-    });
-});
 
-// This function is defined to add accordion functionality
-function addAccordionEventListeners() {
-    const accordions = document.querySelectorAll('.accordion');
-    accordions.forEach(accordion => {
-        accordion.addEventListener('click', function() {
-            this.classList.toggle('active');
-            const panel = this.nextElementSibling;
-            if (panel.style.display === 'block') {
-                panel.style.display = 'none';
+        // 將「本會」的按鈕設為 active
+        const benhuiButton = Array.from(document.querySelectorAll('.contact-n-button')).find(btn => btn.innerText === '本會');
+        if (benhuiButton) {
+            benhuiButton.classList.add('contact-n-active');
+            displayData(data, '本會');
+        }
+    });
+
+
+function displayData(data, continent) {
+    accordionContainer.innerHTML = ''; // Clear previous content
+
+    const filteredData = data.filter(item => item.continents === continent);
+    let uniqueCategories = [];
+
+    if (continent === '本會') {
+        uniqueCategories = [...new Set(filteredData.map(item => item.province))];
+    } else {
+        uniqueCategories = [...new Set(filteredData.map(item => item.nation))];
+    }
+
+    uniqueCategories.forEach(category => {
+        const categoryData = (continent === '本會')
+            ? filteredData.filter(item => item.province === category)
+            : filteredData.filter(item => item.nation === category);
+
+        const button = document.createElement('button');
+        button.className = 'contact-n-accordion';
+        button.innerText = category;
+
+        const div = document.createElement('div');
+        div.className = 'contact-n-panel';
+
+        const table = document.createElement('table');
+        table.className = 'contact-n-table';
+
+        // Add table header
+        const thead = table.createTHead();
+        const headerRow = thead.insertRow();
+        ["志業體名稱", "志業體", "志業體電話", "志業體地址", "志業體地圖"].forEach(header => {
+            const th = document.createElement('th');
+            th.innerText = header;
+            th.className = 'contact-n-th';
+            headerRow.appendChild(th);
+        });
+
+        // Add table body based on the nation
+        categoryData.forEach(entry => {
+            const row = table.insertRow();
+            row.insertCell(0).innerText = entry.affiliateName;
+            row.insertCell(1).innerText = entry.mission;
+            row.insertCell(2).innerText = entry.phone1;
+
+            if (continent === '本會') {
+                row.insertCell(3).innerText = entry.fullAddress;
             } else {
-                panel.style.display = 'block';
+                row.insertCell(3).innerText = entry.address || ''; // Handle null values
+            }
+
+            const mapCell = row.insertCell(4);
+            const mapLink = document.createElement('a');
+            mapLink.href = `https://www.google.com/maps/place/${entry.fullAddress || entry.address}`;
+            mapLink.innerText = 'Google 地圖';
+            mapLink.target = '_blank';
+            mapLink.className = 'contact-n-link';
+            mapCell.appendChild(mapLink);
+        });
+
+        div.appendChild(table);
+
+        accordionContainer.appendChild(button);
+        accordionContainer.appendChild(div);
+    });
+
+    // Accordion functionality
+    const acc = document.getElementsByClassName("contact-n-accordion");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("contact-n-active");
+            const panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
             }
         });
-    });
+    }
 }
